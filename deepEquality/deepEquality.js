f4789ea820a329bfadb43ac7489311e0438756aa
MohamedBelkheirRBK <mohamedbel@protonmail.com>
2019-12-12 09:09:36 +0100

    done with deep equality

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -12,4 +12,47 @@
   *
   */
 var deepEquals = function(apple, orange) {
+  var equal = true;
+
+  function deepSearch(node1, node2) {
+    node1 = Object.entries(node1)
+    node2 = Object.entries(node2)
+
+    //first exit code
+    if(node1.length!=node2.length){
+      equal=false;
+      return true;
+    }
+
+    for(var i in node1) {
+      if(typeof node1[i][1] !== typeof node2[i][1]){
+        equal = false;
+        return true;
+      }
+
+      if(typeof node1[i][1] === 'object'){
+        if(deepSearch(node1[i][1], node2[i][1])){
+          equal = false;
+          return true;
+        }
+      }else
+
+      if(node1[i][0]!==node2[i][0] || node1[i][1]!==node2[i][1]){
+        equal = false;
+        return true;
+      }
+
+
+
+    }
+
+  }
+
+  deepSearch(apple, orange)
+  
+
+  return equal
 };
+
+console.log(deepEquals({a:1, b: {c:3}},{a:1, b: {c:3}})); // true
+console.log(deepEquals({a:1, b: {c:5}},{a:1, b: {c:6}})); // false
