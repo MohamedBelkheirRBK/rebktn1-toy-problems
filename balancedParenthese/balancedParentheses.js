e4d086241bc4e6bf025fde7e90aa6e2ff80b2de4
MohamedBelkheirRBK <mohamedbel@protonmail.com>
2019-12-21 12:04:40 +0300

    refactored balanced parentheses to handle all three types

--- balancedParenthese/balancedParentheses.js
+++ balancedParenthese/balancedParentheses.js
@@ -8,6 +8,103 @@ isBalanced("(50)(")	// false
 isBalanced("") //	true
 */
 
-var isBalanced = function(str) {
-    return str.match(/\(/g).length === str.match(/\)/g).length
-};
+// var isBalanced = function(str) {
+//     var arrayOfStr = str.split('');
+//     var isBalanced = true;
+
+//     var isBalancedParenthesis = function(arr) {
+//         looper(arr, (i)=>{
+//             if(arr[i]===')')
+//                 looper(arr.slice(i+1))
+//             else
+//                 isBalanced = false;
+//         }, ()=>{isBalanced=false;})
+//     }
+
+//     var isBalancedCurlyBrackets = function(arr) {
+//         looper(arr, (i)=>{
+//             if(arr[i]==='}')
+//                 looper(arr.slice(i+1))
+//             else
+//                 isBalanced = false;
+//         }, ()=>{isBalanced=false;})
+//     }
+
+//     var isBalancedSquareBrackets = function(arr) {
+//         looper(arr, (i)=>{
+//             if(arr[i]===']')
+//                 looper(arr.slice(i+1))
+//             else
+//                 isBalanced = false;
+//         },  ()=>{isBalanced=false;})
+//     }
+
+//     function looper(arr, atClosure = ()=>{isBalanced = false;}, atEnd = ()=>{}) {
+//         console.log(arr)
+//         for (var i = 0; i<arr.length; i++) {
+//             switch(arr[i]){
+//                 case "(":
+//                     isBalancedParenthesis(arr.slice(i+1));
+//                     return;
+//                 case "{":
+//                     isBalancedCurlyBrackets(arr.slice(i+1))
+//                     return;
+//                 case "[":
+//                     isBalancedSquareBrackets(arr.slice(i+1))
+//                     return;
+//                 case ")":
+//                 case "]":
+//                 case "}":
+//                     atClosure(i);
+//                     return ;
+//                 default:
+//                     break;
+
+//             }
+//         }
+//         atEnd()
+//     }
+
+//     looper(arrayOfStr)
+
+//     return isBalanced;
+// };
+
+isBalanced = function(str) {
+    var stack = [];
+    var reflect = {
+        ")" : "(",
+        "]" : "[",
+        "}" : "{"
+    }
+        for(var i = 0; i<str.length; i++) {
+            switch(str[i]){
+                case "(":
+                case "{":
+                case "[":
+                    stack.push(str[i])
+                    break;
+                case ")":
+                case "]":
+                case "}":
+                    if(stack.pop()!==reflect[str[i]]){
+                        return false;
+                    }
+                    break;
+                default:
+                    break;
+
+            }
+        }
+
+        if(stack.length){
+
+            return false;}
+        
+        return true;
+
+    
+}
+
+
+console.log(isBalanced("(({)})"))
