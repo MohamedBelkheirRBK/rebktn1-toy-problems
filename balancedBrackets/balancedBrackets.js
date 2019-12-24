cd20d93c00adee4ec63818ef101db54b9b88f1d8
MohamedBelkheirRBK <mohamedbel@protonmail.com>
2019-12-24 08:50:51 +0100

    done this already

--- balancedBrackets/balancedBrackets.js
+++ balancedBrackets/balancedBrackets.js
@@ -9,4 +9,36 @@ isBalanced("(50)(") // false
 isBalanced("[{]}") // false
 */
 
-var isBalanced = function(str) {};
+var isBalanced = function(str) {
+    var stack = [];
+    var reflect = {
+        ")" : "(",
+        "]" : "[",
+        "}" : "{"
+    }
+        for(var i = 0; i<str.length; i++) {
+            switch(str[i]){
+                case "(":
+                case "{":
+                case "[":
+                    stack.push(str[i])
+                    break;
+                case ")":
+                case "]":
+                case "}":
+                    if(stack.pop()!==reflect[str[i]]){
+                        return false;
+                    }
+                    break;
+                default:
+                    break;
+
+            }
+        }
+
+        if(stack.length){
+
+            return false;}
+        
+        return true;
+};
