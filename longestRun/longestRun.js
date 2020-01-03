8052b4849667594bbf3eabac3d0df7b1624f29b1
MohamedBelkheirRBK <mohamedbel@protonmail.com>
2020-01-03 09:01:43 +0100

    done with longest run

--- longestRun/longestRun.js
+++ longestRun/longestRun.js
@@ -12,4 +12,37 @@ longestRun("abcdefgh") // [ 0, 0 ]
 longestRun("abccccccc") // [ 2, 8 ]
 */
 
-function longestRun(string) {}
+function longestRun(string) {
+    var wrry = [0, 0];
+    var current = [0, 0];
+    var started = false;
+    for (var c=0; c<string.length; c++) {
+        console.log("Iteration #", c, " at character "+string[c])
+        if (string[c]===string[c-1]) {
+            console.log("character before "+string[c]+" is equal to it")
+            if (!started) {
+                current[0] = c-1;
+                started = true;
+                console.log("started counting")
+            }
+
+            current[1] = c;
+
+        } else {
+            console.log(" previous character is not equal to it")
+            if (started) {
+                console.log("stopped counting")
+                started = false;
+            }
+        }
+        console.log("----------------- end of iteration --------------------")
+        if (wrry[1] - wrry[0] < current[1] - current[0]) {
+            wrry = [...current]
+        }
+
+    }
+
+    return wrry;
+}
+
+console.log("result: ", longestRun("abccccccc"))
