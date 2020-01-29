4c310eafc17a2ee5c60d1d2d89a94a5b2c5b4df4
MohamedBelkheirRBK <mohamedbel@protonmail.com>
2020-01-29 09:20:29 +0100

    done with longest plaindrome

--- longestPalindrome/longestPalindrome.js
+++ longestPalindrome/longestPalindrome.js
@@ -6,5 +6,36 @@ Other palindromes in the above string include "dad", "ete", " dad " (including w
 */
 
 function longestPalindrome(string) {
-  // your code here...
+  string = string.split("");
+  var palindromes = [];
+  
+  for(j in string) {
+    var passed = [];
+    for(var c of string.slice(j)) {
+      passed.push(c);
+      var start = 0;
+      var end = passed.length - 1;
+      var matching = true;
+      while (start < end) {
+        if(passed[start]!==passed[end]){
+          matching = false;
+        }
+        start++;
+        end--;
+      }
+      if(matching)
+        palindromes.push([...passed])
+    }
+  }
+
+  return palindromes.reduce((max, curr)=>{
+    if(curr.length>max.length)
+      return curr
+    return max
+  }).join("")
+
 }
+
+console.log(longestPalindrome("My dad is a racecar athlete"))
+
+// X O X O X O X
