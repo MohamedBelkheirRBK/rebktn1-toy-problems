e771980f07a68a9997fc0cf5b7fdfd66b89e495e
MohamedBelkheirRBK <mohamedbel@protonmail.com>
2020-02-15 09:02:52 +0100

    finished implement operators

--- implementOperators/implementOperators.js
+++ implementOperators/implementOperators.js
@@ -12,13 +12,25 @@ EXAMPLES:
 */
 
 var multiply = function(x, y) {
-  // your code here...
+  for(var i = y; i!=0; i--){
+    x+=x;
+  }
+  return x;
 };
 
 var divide = function(x, y) {
-  // your code here...
+  if(y <= 0) throw Error("can't divide by zero")
+  var result = 0;
+  var leftover = 0;
+  while(x - y >= 0) {
+    x-=y;
+    result++;
+  }
+  leftover = x;
+  return [result, leftover]
 };
 
 var modulo = function(x, y) {
-  // your code here...
+  var [_, result] = divide(x, y);
+  return result;
 };
